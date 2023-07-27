cbea59b4def9f81b7a1db1f5ebe2c2e8a0ceb010
LowellC86 <lowellcaraballo@gmail.com>
2023-07-27 18:59:21 -0400

    collect static

--- cjmonet_project/cjmonet_django/settings.py
+++ cjmonet_project/cjmonet_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-sxje3*^7l-uf2srj(su31g2qte8#t2@*)%z__&o%r7457z6tv)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -147,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
